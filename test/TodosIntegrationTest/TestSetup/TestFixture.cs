674e1e62c987de69e0578ad140c7594e6a8e8aae
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-02-11 00:40:58 +0330

    Add integration test project.

---
+++ test/TodosIntegrationTest/TestSetup/TestFixture.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace TodosIntegrationTest.TestSetup
+{
+    public class TestFixture<TProgram> where TProgram : class
+    {
+        public TestFixture()
+        {
+            var methodInfo = typeof(TProgram).GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static);
+            string[] args = { };
+            methodInfo.Invoke(null, new[] { args });
+        }
+    }
+}
