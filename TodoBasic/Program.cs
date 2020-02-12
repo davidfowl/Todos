64d1e99503718c5d4e4118678244469154dd5f27
o.musin <o.musin@tinkoff.ru>
2020-02-12 13:08:39 +0300

    added dockerfiles + dockercompose

--- TodoBasic/Program.cs
+++ TodoBasic/Program.cs
@@ -2,7 +2,6 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 
 namespace Todos
