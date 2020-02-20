f5f48ccd42480bbc9dc4c5c90deb1556c7a535e5
soeleman <soeleman@gmail.com>
2020-02-20 18:06:31 +0700

    shared folder to package on Feedz.io

--- Shared/Configuration/GenericApiConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Microsoft.AspNetCore.Builder
-{
-    public sealed class GenericApiConfiguration
-    {
-        public HashSet<GenericApiControllerConfiguration> Controllers { get; set; } = new HashSet<GenericApiControllerConfiguration>();
-    }
-}
--- Shared/Configuration/GenericApiEfCoreConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Microsoft.AspNetCore.Builder
-{
-    public sealed class GenericApiEfCoreConfiguration
-    {
-        public HashSet<string> DbSetTypes { get; set; } = new HashSet<string>();
-    }
-}
