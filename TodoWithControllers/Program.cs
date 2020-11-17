b26275538fbcfbd286228a37e62a77fee716177c
David Fowler <davidfowl@gmail.com>
2020-11-17 17:39:28 -0400

    Single file MVC

--- TodoWithControllers/Program.cs
+++ TodoWithControllers/Program.cs
@@ -1,8 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Todos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,3 +16,72 @@ var app = builder.Build();
 app.MapControllers();
 
 await app.RunAsync();
+
+[ApiController]
+[Route("/api/todos")]
+public class TodoController : ControllerBase
+{
+    private readonly TodoDbContext _db;
+    public TodoController(TodoDbContext db)
+    {
+        _db = db ?? throw new ArgumentNullException(nameof(db));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Todo>>> GetAll()
+    {
+        var todos = await _db.Todos.ToListAsync();
+
+        return todos;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Todo>> Get(long id)
+    {
+        var todo = await _db.Todos.FindAsync(id);
+        if (todo == null)
+        {
+            return NotFound();
+        }
+
+        return todo;
+    }
+
+    [HttpPost]
+    public async Task Post(Todo todo)
+    {
+        _db.Todos.Add(todo);
+        await _db.SaveChangesAsync();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(long id)
+    {
+        var todo = await _db.Todos.FindAsync(id);
+        if (todo == null)
+        {
+            return NotFound();
+        }
+
+        _db.Todos.Remove(todo);
+        await _db.SaveChangesAsync();
+        return Ok();
+    }
+}
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+}
--- TodoWithControllers/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Todos
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
